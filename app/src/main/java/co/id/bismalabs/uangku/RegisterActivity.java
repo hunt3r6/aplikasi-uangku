6359729159e067ad2adaeea58be67ab3e4e028ca
hunt3r6 <faisalmahadi126@gmail.com>
2019-11-11 11:03:52 +0700

    ADD: Register Activity

---
+++ app/src/main/java/co/id/bismalabs/uangku/RegisterActivity.java
@@ -0,0 +1,26 @@
+package co.id.bismalabs.uangku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+    TextView tvLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        tvLogin = findViewById(R.id.tvLogin);
+        tvLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
